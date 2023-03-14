57bb266c1ce05cf6af88245d7342a47191e2ba83
Canberk <renticar@RentiCar-MacBook-Pro.local>
2023-03-14 13:08:47 +0300

    route structure: product page

---
+++ src/pages/[seller]/[slug].js
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./Product.module.css";
+import Navbar from "../../layouts";
+import { ProductSection, CommentsSection, Carousel } from "../../components";
+
+function Product() {
+  return (
+    <div>
+      <div className={style.container}>
+        <ProductSection />
+
+        <div className={style.similarProducts}>
+          <h3>Beğenebileceğiniz Ürünler</h3>
+          <Carousel />
+        </div>
+
+        <div className={style.commentsArea}>
+          <h3>Ürün Değerlendirmeleri</h3>
+          <CommentsSection />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
